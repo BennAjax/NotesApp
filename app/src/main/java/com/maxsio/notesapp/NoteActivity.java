dfa9dc333a15f8eac85be7a2db07a94240219e6b
BennAjax <bennkeys1@gmail.com>
2020-09-07 19:30:46 +0100

    populate spinner

--- app/src/main/java/com/maxsio/notesapp/NoteActivity.java
+++ app/src/main/java/com/maxsio/notesapp/NoteActivity.java
@@ -1,18 +1,15 @@
 package com.maxsio.notesapp;
 
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.view.View;
-
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Spinner;
+import java.util.List;
 
 public class NoteActivity extends AppCompatActivity {
 
@@ -24,6 +21,11 @@ public class NoteActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         Spinner spinnerCourses = findViewById(R.id.spinner_courses);
+        List<CourseInfo> courses = DataManager.getInstance().getCourses();
+        ArrayAdapter<CourseInfo> adapterCourses =
+                new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, courses);
+        adapterCourses.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerCourses.setAdapter(adapterCourses);
 
     }
 
